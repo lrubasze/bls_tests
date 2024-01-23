f5b530f0a869e31c7fb70224061e3ffbd410f459
Lukasz Rubaszewski <lukasz.rubaszewski@rdx.works>
2024-01-23 12:15:49 +0100

    Tweak test data

--- src/cli.rs
+++ src/cli.rs
@@ -25,11 +25,25 @@ const CRYPTO_SCRYPTO_BLUEPRINT_NAME: &str = "CryptoScrypto";
 const CRYPTO_SCRYPTO_PACKAGE_ADDRESS: &str =
     "package_tdx_21_1pkt7zdllsneytdc9g60xn9jjhhwx7jaqxmeh58l4dwyx7rt5z9428f";
 
-const TEST_MSG: &str = "Hello World!";
-const _TEST_MSG_HASH: &str = "3ea2f1d0abf3fc66cf29eebb70cbd4e7fe762ef8a09bcc06c8edf641230afec0";
+const TEST_MSG1: &str = "Hello World!";
+const TEST_MSG2: &str = "Goodbye World!";
 // Below key is derived from secret key: 5B00CC8C7153F39EF2E6E2FADB1BB95A1F4BF21F43CC5B28EFA9E526FB788C08
-const TEST_PUB_KEY: &str = "8a38419cb83c15a92d11243384bea0acd15cbacc24b385b9c577b17272d6ad68bb53c52dbbf79324005528d2d73c2643";
-const TEST_SIGNATURE: &str = "82131f69b6699755f830e29d6ed41cbf759591a2ab598aa4e9686113341118d1db900d190436048601791121b5757c341045d4d0c94a95ec31a9ba6205f9b7504de85dadff52874375c58eec6cec28397279de87d5595101e398d31646d345bb";
+const TEST_PUB_KEY1: &str = "8a38419cb83c15a92d11243384bea0acd15cbacc24b385b9c577b17272d6ad68bb53c52dbbf79324005528d2d73c2643";
+
+// Below key is derived from secret key: Bls12381G1PrivateKey::from_u64(2)
+const TEST_PUB_KEY2: &str = "a572cbea904d67468808c8eb50a9450c9721db309128012543902d0ac358a62ae28f75bb8f1c7c42c39a8c5529bf0f4e";
+
+// signed Keccak256(TEST_MSG1) with Secret key 5B00CC8C7153F39EF2E6E2FADB1BB95A1F4BF21F43CC5B28EFA9E526FB788C08
+const TEST_KCK_MSG1_SIGNATURE: &str = "82131f69b6699755f830e29d6ed41cbf759591a2ab598aa4e9686113341118d1db900d190436048601791121b5757c341045d4d0c94a95ec31a9ba6205f9b7504de85dadff52874375c58eec6cec28397279de87d5595101e398d31646d345bb";
+
+// signed TEST_MSG1 with Secret key 5B00CC8C7153F39EF2E6E2FADB1BB95A1F4BF21F43CC5B28EFA9E526FB788C08
+const TEST_MSG1_SIGNATURE: &str = "ae3f65cc78ea4668c716837e07b4d6115b8304a01e0b9733b52f90c84d9b853e4521314f39f0bab1bd458c9f586248f0026c15c816b5a3cfe026837552676a972d3a9f329c1ab8a3d1d6b685d9a46b6b1bceedce6269929c71890fd44028bfc0";
+// signed TEST_MSG2 with Secret key Bls12381G1PrivateKey::from_u64(2)
+const TEST_MSG2_SIGNATURE: &str = "a077aff3fbee37fc7a260d4b955626b390d4b0d9604e06d2c3566d24c7adf1009ed3898142ed8b26af4064ed562fbd1f048c8261a748591874e166e97a23bfb8929117a74891640458414aede9ec02ae1a7c602f41dc928045846015d8148d5a";
+
+const TEST_AGGREGATE_VERIFY_SIGNATURE: &str = "a9e876f875b09c22f222b9052f339bdfc94529d3f409ed7476885c1c68acab6d02d686b6a9b585b224d3246e844e55911235b1e640b01cc864b2b3e8ddc961e5f2f69f91e8c11c7517f4cdf51cb33a828a1a5f457faef13463b6714735ecccac";
+
+const TEST_FAST_AGGREGATE_VERIFY_SIGNATURE: &str = "86aab079e63f469ee176982f4ab42d367f2a6ecd6e389f31c4b214c37581f1cb9e71203693364c2fc85e361d28fbdefb03773301363ae1649af0e1a23cfdc65a73ec8c4463bd89399a38de8f9a85d85a504721d96fac2b6df0a17722edc61f6e";
 
 const CRYPTO_SCRYPTO_CODE_PATH: &str = "crypto_scrypto/crypto_scrypto.wasm";
 const CRYPTO_SCRYPTO_RPD_PATH: &str = "crypto_scrypto/crypto_scrypto.rpd";
@@ -77,7 +91,7 @@ struct KeccakHash {
     #[arg(long, short = 'a', default_value_t = CRYPTO_SCRYPTO_PACKAGE_ADDRESS.to_string())]
     /// Package address of the CryptoScrypto blueprint
     package_address: String,
-    #[arg(long, short, default_value_t = TEST_MSG.to_string())]
+    #[arg(long, short, default_value_t = TEST_MSG1.to_string())]
     /// Message to hash
     msg: String,
 }
@@ -87,14 +101,14 @@ struct BlsVerify {
     #[arg(long, short = 'a', default_value_t = CRYPTO_SCRYPTO_PACKAGE_ADDRESS.to_string())]
     /// Package address of the CryptoScrypto blueprint
     package_address: String,
-    #[arg(long, short, default_value_t = TEST_MSG.to_string())]
+    #[arg(long, short, default_value_t = TEST_MSG1.to_string())]
     /// Message to verify signature with (it will be hashed before with Keccak256)
     msg: String,
-    #[arg(long, short, default_value_t = TEST_PUB_KEY.to_string())]
+    #[arg(long, short, default_value_t = TEST_PUB_KEY1.to_string())]
     /// BLS public key to perform verification (hex-encoded string)
     public_key: String,
     /// BLS signature to verify (hex-encoded string)
-    #[arg(long, short, default_value_t = TEST_SIGNATURE.to_string())]
+    #[arg(long, short, default_value_t = TEST_KCK_MSG1_SIGNATURE.to_string())]
     signature: String,
 }
 
@@ -103,14 +117,14 @@ struct BlsAggregateVerify {
     #[arg(long, short = 'a', default_value_t = CRYPTO_SCRYPTO_PACKAGE_ADDRESS.to_string())]
     /// Package address of the CryptoScrypto blueprint
     package_address: String,
-    #[arg(long, short, use_value_delimiter = true, value_delimiter = ',', default_values_t = vec![TEST_MSG.to_string()])]
-    /// Messages to verify signature with (it will be hashed before with Keccak256)
+    #[arg(long, short, use_value_delimiter = true, value_delimiter = ',', default_values_t = vec![TEST_MSG1.to_string(), TEST_MSG2.to_string()])]
+    /// Messages to verify signature with
     msgs: Vec<String>,
-    #[arg(long, short, use_value_delimiter = true, value_delimiter = ',', default_values_t = vec![TEST_PUB_KEY.to_string()])]
+    #[arg(long, short, use_value_delimiter = true, value_delimiter = ',', default_values_t = vec![TEST_PUB_KEY1.to_string(), TEST_PUB_KEY2.to_string()])]
     /// BLS public key to perform verification (hex-encoded string)
     public_keys: Vec<String>,
     /// BLS signature to verify (hex-encoded string)
-    #[arg(long, short, default_value_t = TEST_SIGNATURE.to_string())]
+    #[arg(long, short, default_value_t = TEST_AGGREGATE_VERIFY_SIGNATURE.to_string())]
     signature: String,
 }
 
@@ -119,14 +133,14 @@ struct BlsFastAggregateVerify {
     #[arg(long, short = 'a', default_value_t = CRYPTO_SCRYPTO_PACKAGE_ADDRESS.to_string())]
     /// Package address of the CryptoScrypto blueprint
     package_address: String,
-    #[arg(long, short, default_value_t = TEST_MSG.to_string())]
-    /// Message to verify signature with (it will be hashed before with Keccak256)
+    #[arg(long, short, default_value_t = TEST_MSG1.to_string())]
+    /// Message to verify signature with
     msg: String,
-    #[arg(long, short, use_value_delimiter = true, value_delimiter = ',', default_values_t = vec![TEST_PUB_KEY.to_string()])]
+    #[arg(long, short, use_value_delimiter = true, value_delimiter = ',', default_values_t = vec![TEST_PUB_KEY1.to_string(), TEST_PUB_KEY2.to_string()])]
     /// BLS public key to perform verification (hex-encoded string)
     public_keys: Vec<String>,
     /// BLS signature to verify (hex-encoded string)
-    #[arg(long, short, default_value_t = TEST_SIGNATURE.to_string())]
+    #[arg(long, short, default_value_t = TEST_FAST_AGGREGATE_VERIFY_SIGNATURE.to_string())]
     signature: String,
 }
 
@@ -136,7 +150,7 @@ struct BlsSignatureAggregate {
     /// Package address of the CryptoScrypto blueprint
     package_address: String,
     /// BLS signatures to aggregate (hex-encoded string)
-    #[arg(long, short, use_value_delimiter = true, value_delimiter = ',', default_values_t = vec![TEST_SIGNATURE.to_string()])]
+    #[arg(long, short, use_value_delimiter = true, value_delimiter = ',', default_values_t = vec![TEST_MSG1_SIGNATURE.to_string(), TEST_MSG2_SIGNATURE.to_string()])]
     signatures: Vec<String>,
 }
 
@@ -247,7 +261,8 @@ impl CliCtx {
     fn cmd_keccak_hash(&self, cmd: &KeccakHash) {
         // Convert address from the human-readable bech32 format
         let package_address =
-            PackageAddress::try_from_bech32(&self.address_decoder, &cmd.package_address).unwrap();
+            PackageAddress::try_from_bech32(&self.address_decoder, &cmd.package_address)
+                .expect("Invalid package address");
         let data = cmd.msg.as_bytes().to_vec();
 
         println!("Package address : {}", cmd.package_address);
@@ -287,7 +302,8 @@ impl CliCtx {
     fn cmd_bls_verify(&self, cmd: &BlsVerify) {
         // Convert address from the human-readable bech32 format
         let package_address =
-            PackageAddress::try_from_bech32(&self.address_decoder, &cmd.package_address).unwrap();
+            PackageAddress::try_from_bech32(&self.address_decoder, &cmd.package_address)
+                .expect("Invalid package address");
         let msg_hash = keccak256_hash(cmd.msg.clone());
 
         println!("Package address : {}", cmd.package_address);
@@ -375,7 +391,8 @@ impl CliCtx {
     fn cmd_bls_aggregate_verify(&self, cmd: &BlsAggregateVerify) {
         // Convert address from the human-readable bech32 format
         let package_address =
-            PackageAddress::try_from_bech32(&self.address_decoder, &cmd.package_address).unwrap();
+            PackageAddress::try_from_bech32(&self.address_decoder, &cmd.package_address)
+                .expect("Invalid package address");
 
         println!("Package address : {}", cmd.package_address);
         println!("Messages        : {:?}", cmd.msgs);
@@ -424,7 +441,8 @@ impl CliCtx {
     fn cmd_bls_fast_aggregate_verify(&self, cmd: &BlsFastAggregateVerify) {
         // Convert address from the human-readable bech32 format
         let package_address =
-            PackageAddress::try_from_bech32(&self.address_decoder, &cmd.package_address).unwrap();
+            PackageAddress::try_from_bech32(&self.address_decoder, &cmd.package_address)
+                .expect("Invalid package address");
 
         println!("Package address : {}", cmd.package_address);
         println!("Message         : {:?}", cmd.msg);
@@ -474,7 +492,8 @@ impl CliCtx {
     fn cmd_bls_signature_aggregate(&self, cmd: &BlsSignatureAggregate) {
         // Convert address from the human-readable bech32 format
         let package_address =
-            PackageAddress::try_from_bech32(&self.address_decoder, &cmd.package_address).unwrap();
+            PackageAddress::try_from_bech32(&self.address_decoder, &cmd.package_address)
+                .expect("Invalid package address");
 
         println!("Package address : {}", cmd.package_address);
         println!("Signatures      : {:?}", cmd.signatures);
@@ -507,7 +526,7 @@ impl CliCtx {
             // - then decode SBOR to the expected type
             let sbor_data = hex::decode(output).unwrap();
 
-            let result: bool = scrypto_decode(&sbor_data).unwrap();
+            let result: Bls12381G2Signature = scrypto_decode(&sbor_data).unwrap();
             println!("BLS signature aggregate  : {:?}", result);
         } else {
             let error = details.get_error().unwrap();
